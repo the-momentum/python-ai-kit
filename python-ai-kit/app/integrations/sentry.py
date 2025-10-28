e8282fefb2ae3e3cfdbee6374d68c67a3c38e441
KaliszS <sk.kaliszs@gmail.com>
2025-10-28 20:34:17 +0100

    Add sentry plugin

---
+++ python-ai-kit/app/integrations/sentry.py
@@ -0,0 +1,12 @@
+import sentry_sdk
+
+from app.config import settings
+
+
+def init_sentry():
+    if settings.SENTRY_ENABLED:
+        sentry_sdk.init(
+            dsn=settings.SENTRY_DSN,
+            environment=settings.SENTRY_ENV,
+            traces_sample_rate=settings.SENTRY_SAMPLES_RATE,
+        )
