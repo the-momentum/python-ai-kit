d83b05f42624c63f7022924d1a4eb9fbd4434046
Filip Begiełło <filip.begiello@applover.pl>
2025-11-18 11:53:55 +0100

    [fix] Celery related cleanup

--- python-ai-kit/app/integrations/celery/core.py
+++ python-ai-kit/app/integrations/celery/core.py
@@ -20,11 +20,11 @@ def create_celery() -> Celery:
         result_expires=3 * 24 * 3600,
     )
 
-    celery_app.autodiscover_tasks(["app.integrations.celery.tasks.dummy_task"])
+    celery_app.autodiscover_tasks(["app.integrations.celery"])
 
     celery_app.conf.beat_schedule = {
         "dummy-task": {
-            "task": "app.integrations.celery.tasks.dummy_task",
+            "task": "app.integrations.celery.tasks.dummy_task.dummy_task",
             "schedule": crontab(minute="*/1"),
         },
     }
